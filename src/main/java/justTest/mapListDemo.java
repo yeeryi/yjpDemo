187b16315e2a750e1cf29a2853bb5695727d1977
yeeryi <980077360@qq.com>
2020-05-06 18:00:31 +0800

    map,list,set

---
+++ src/main/java/justTest/mapListDemo.java
@@ -0,0 +1,18 @@
+package justTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Author:YeJianPeng<br/>
+ * Date:2020/05/06<br/>
+ * Time:11:02<br/>
+ */
+public class mapListDemo {
+    //这里面将是map，list，set
+    public static void main(String[] args) {
+        Map map =new HashMap<>();
+
+
+    }
+}
